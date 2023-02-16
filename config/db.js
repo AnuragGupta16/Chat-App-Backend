a837d81e8a45d20c71a9add329dfcc53a0c95559
Anurag Gupta <86673132+AnuragGupta16@users.noreply.github.com>
2023-02-16 16:31:17 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
+mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: true,
+      useUnifiedTopology: true
+     
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
