17f79bb3d59701aaa35a77fd8a32b648cc2b3515
Anurag Gupta <86673132+AnuragGupta16@users.noreply.github.com>
2023-02-17 16:27:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,33 @@ const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const path = require("path");
+const cors = require('cors');
+// Cors 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
+const corsOpts = {
+  origin: '*',
 
+  methods: [
+    'GET',
+    'POST',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+// Default configuration looks like
+// {
+//     "origin": "*",
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+//   }
+app.use(cors(corsOpts));
+app.use(cors(corsOptions))
 dotenv.config();
 connectDB();
 const app = express();
