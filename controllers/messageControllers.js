51b7832137ab5945893992b904e30c824eebfd6e
Anurag Gupta <86673132+AnuragGupta16@users.noreply.github.com>
2023-02-20 01:26:45 +0530

    Update messageControllers.js

--- controllers/messageControllers.js
+++ controllers/messageControllers.js
@@ -38,8 +38,8 @@ const sendMessage = asyncHandler(async (req, res) => {
   try {
     var message = await Message.create(newMessage);
 
-    message = await message.populate("sender", "name pic").execPopulate();
-    message = await message.populate("chat").execPopulate();
+    message = await message.populate("sender", "name pic");
+    message = await message.populate("chat");
     message = await User.populate(message, {
       path: "chat.users",
       select: "name pic email",
