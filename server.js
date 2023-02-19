f8c0d105849e917ef99fe5d0dd27e5385cc8458e
Anurag Gupta <86673132+AnuragGupta16@users.noreply.github.com>
2023-02-19 23:36:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require("path");
 const cors = require('cors');
 // Cors 
 const corsOptions = {
-  origin:'https://0talk-a-tive0.netlify.app'
+  origin:process.env.ALLOWED_CLIENTS.split(',')
   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 }
 const corsOpts = {
